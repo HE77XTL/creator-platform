b843aa472892d15a6f9635c52a4b6c95b5fff05b
heds <he77505357@163.com>
2022-08-06 22:18:29 +0800

    init

---
+++ src/main/java/com/heds/creatorplatform/CreatorPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.heds.creatorplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heds/creatorplatform/CreatorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heds.creatorplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
